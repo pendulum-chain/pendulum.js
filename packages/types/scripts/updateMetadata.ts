d509fa3835309cbd2aceb265fe481a97c9d7028a
Marcel Ebert <mail@marcel-ebert.de>
2023-01-16 15:55:57 +0100

    Change updateMetadata script

--- packages/types/scripts/updateMetadata.ts
+++ packages/types/scripts/updateMetadata.ts
@@ -17,7 +17,7 @@ const main = (): void => {
   ws.onmessage = (msg: any): void => {
     const metadata = JSON.parse(msg.data).result;
     fs.writeFileSync('packages/types/src/metadata/static-latest.ts', `export default '${metadata}'`);
-    fs.writeFileSync('packages/types/src/metadata/static-latest.json', msg.data);
+    fs.writeFileSync('packages/types/src/metadata/static-latest-json.json', msg.data);
     console.log('Done');
     process.exit(0);
   };
